901c7db2b060dcf954ff63e0434e86f439fb305e
Jannis Baum <jannis.baum@gmail.com>
2024-07-07 02:15:34 +0200

    feat: syncing

--- src/sync.py
+++ src/sync.py
@@ -16,9 +16,18 @@ class JupySync:
         self.py = temp + '.py'
 
     def __enter__(self):
+        # we work with a copied notebook for syncing to avoid adding jupytext
+        # metadata to the original and/or version control
         shutil.copy(self.nb_original, self.nb_copy)
         jupytext(['--set-formats', 'ipynb,py:percent', self.nb_copy])
+        return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         os.remove(self.nb_copy)
         os.remove(self.py)
+
+    def sync(self):
+        # sync to copied notebook, copy synced notebook, remove metadata
+        jupytext(['--sync', self.py])
+        shutil.copy(self.nb_copy, self.nb_original)
+        jupytext([self.nb_original, '--update-metadata', '{"jupytext":null}'])
