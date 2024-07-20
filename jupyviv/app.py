a2db9fd05c5786a3cce3b950a46b80c4b08a7739
Jannis Baum <jannis.baum@gmail.com>
2024-07-20 18:30:06 +0200

    feat: run code

--- jupyviv/app.py
+++ jupyviv/app.py
@@ -2,17 +2,39 @@
 
 from typing import Callable
 
-from bottle import Bottle
+from bottle import Bottle, abort
 
 from jupyviv.sync import JupySync
+from jupyviv.kernel import Kernel
 
-def setup_bottle(jupy_sync: JupySync, reload: Callable[[], None]):
+_success = 'success'
+
+def setup_bottle(jupy_sync: JupySync, kernel: Kernel, reload: Callable[[], None]):
     app = Bottle()
 
     @app.post('/sync')
     def sync():
         jupy_sync.sync()
         reload()
-        return 'success'
+        return _success
+
+    @app.post('/run/<line>')
+    def run(line: str):
+        print(line)
+        try:
+            line_i = int(line)
+            if line_i < 0 or line_i >= len(jupy_sync.line2cell):
+                raise ValueError
+        except ValueError:
+            print('abort')
+            abort(400, 'Invalid line number')
+            return
+
+        cell_i = jupy_sync.line2cell[line_i]
+        code = jupy_sync.code_for_cell(cell_i)
+        exec_count, outputs = kernel.execute(code)
+        jupy_sync.set_cell_exec_data(cell_i, exec_count, outputs)
+        sync()
+        return _success
 
     return app
--- jupyviv/cli.py
+++ jupyviv/cli.py
@@ -4,6 +4,7 @@ import logging
 from bottle import run
 
 from jupyviv.app import setup_bottle
+from jupyviv.kernel import Kernel
 from jupyviv.sync import JupySync
 from jupyviv.vivify import viv_open, viv_port, viv_reload
 
@@ -16,7 +17,7 @@ def cli():
 
     logging.basicConfig(level=args.log)
 
-    with JupySync(args.notebook) as jupy_sync:
+    with JupySync(args.notebook) as jupy_sync, Kernel() as kernel:
         viv_open(args.notebook)
-        app = setup_bottle(jupy_sync, lambda: viv_reload(args.notebook))
+        app = setup_bottle(jupy_sync, kernel, lambda: viv_reload(args.notebook))
         run(app, host='localhost', port=viv_port + 1)
