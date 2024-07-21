276f45085718b6197131ac783a35ce83b6879032
Jannis Baum <jannis.baum@gmail.com>
2024-07-21 11:10:36 +0200

    feat: handle startup errors

--- jupyviv/cli.py
+++ jupyviv/cli.py
@@ -1,12 +1,13 @@
 import argparse
 
-from jupyviv.communication import run
+from jupyviv.communication import JupyVivError, run
 from jupyviv.endpoints import setup_endpoints
 from jupyviv.kernel import Kernel
-from jupyviv.logs import set_loglevel
+from jupyviv.logs import set_loglevel, get_logger
 from jupyviv.sync import JupySync
 from jupyviv.vivify import viv_open, viv_reload
 
+logger = get_logger(__name__)
 
 def cli():
     parser = argparse.ArgumentParser()
@@ -16,7 +17,11 @@ def cli():
 
     set_loglevel(args.log)
 
-    with JupySync(args.notebook) as jupy_sync, Kernel() as kernel:
-        viv_open(args.notebook)
-        endpoints = setup_endpoints(jupy_sync, kernel, lambda: viv_reload(args.notebook))
-        run(endpoints)
+    try:
+        with JupySync(args.notebook) as jupy_sync, Kernel() as kernel:
+            viv_open(args.notebook)
+            endpoints = setup_endpoints(jupy_sync, kernel, lambda: viv_reload(args.notebook))
+            run(endpoints)
+    except JupyVivError as e:
+        logger.critical(e)
+        exit(1)
