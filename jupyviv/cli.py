a4b14b5b5d73a51258674224f6e554249a03e72a
Jannis Baum <jannis.baum@gmail.com>
2025-03-08 08:39:21 +0100

    feat(#1): subcommand setup

--- jupyviv/cli.py
+++ jupyviv/cli.py
@@ -1,27 +1,23 @@
 import argparse
 
-from jupyviv.communication import JupyVivError, run
-from jupyviv.endpoints import setup_endpoints
-from jupyviv.kernel import Kernel
-from jupyviv.logs import set_loglevel, get_logger
-from jupyviv.sync import JupySync
-from jupyviv.vivify import viv_open, viv_reload
+from jupyviv.shared.logs import set_loglevel
 
-logger = get_logger(__name__)
+type Subparsers = argparse._SubParsersAction[argparse.ArgumentParser]
 
 def cli():
     parser = argparse.ArgumentParser()
-    parser.add_argument('notebook', type=str)
     parser.add_argument('--log', type=str, default='WARNING', help='Log level')
+    parser.print_help
+
+    # subparsers are passed to modules to add their own subcommands
+    # have to specify 'args.func' to run the subcommand
+    subparsers = parser.add_subparsers(help='Subcommand')
+
     args = parser.parse_args()
 
     set_loglevel(args.log)
 
-    try:
-        with JupySync(args.notebook) as jupy_sync, Kernel(jupy_sync.kernel_name) as kernel:
-            viv_open(args.notebook)
-            endpoints = setup_endpoints(jupy_sync, kernel, lambda: viv_reload(args.notebook))
-            run(endpoints)
-    except JupyVivError as e:
-        logger.critical(e)
-        exit(1)
+    if hasattr(args, 'func'):
+        return args.func(args)
+    parser.print_help()
+    return 1
