6c348b090daa4e0afa8e83c5db0c4a8480b35c31
Jannis Baum <jannis.baum@gmail.com>
2025-03-09 17:53:05 +0100

    feat(#1): start new async kernel

--- src/jupyviv/agent/kernel.py
+++ src/jupyviv/agent/kernel.py
@@ -1,57 +1,45 @@
-from jupyter_client.blocking.client import BlockingKernelClient
-from jupyter_client.manager import KernelManager
+from typing import Awaitable, Callable
+from jupyter_client.manager import start_new_async_kernel
 
 from jupyviv.shared.logs import get_logger
+from jupyviv.shared.messages import AsyncMessageHandler, AsyncMessageQueue, Message
 from jupyviv.shared.utils import dsafe
 
 _logger = get_logger(__name__)
 
-class Kernel:
-    _output_msg_types = ['execute_result', 'display_data', 'stream', 'error']
+# returns message handler & runner for kernel
+async def setup_kernel(name: str, send_queue: AsyncMessageQueue) -> tuple[AsyncMessageHandler, Callable[[], Awaitable[None]]]:
+    _logger.info(f'Starting kernel {name}')
+    km, kc = await start_new_async_kernel(kernel_name=name)
+    _logger.info(f'Kernel ready')
 
-    def __init__(self, name):
-        self.name = name
+    id_kernel2jupyviv = dict[str, int]()
 
-    def __enter__(self):
-        _logger.info('Starting kernel')
-        self._km: KernelManager = KernelManager(kernel_name=self.name)
-        self._km.start_kernel()
-
-        self._kc: BlockingKernelClient = self._km.client()
-        self._kc.start_channels()
-        self._kc.wait_for_ready()
-        _logger.info('Kernel ready')
-
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        if self._kc is not None:
-            self._kc.stop_channels()
-        if self._km is not None:
-            self._km.shutdown_kernel()
-        del self._kc
-        del self._km
-
-    # returns execution count & outputs
-    def execute(self, code) -> tuple[int | None, list]:
-        msg_id = self._kc.execute(code)
-        messages = list()
-        execution_count = None
+    async def _handle_kernel_messages():
         while True:
-            try:
-                msg = self._kc.get_iopub_msg()
-                # message belongs to our execution request
-                if dsafe(msg, 'parent_header', 'msg_id') == msg_id:
-                    msg_type = dsafe(msg, 'msg_type')
-                    content = dsafe(msg, 'content')
-                    # kernel is back in idle -> done
-                    if msg_type == 'status' and dsafe(content, 'execution_state') == 'idle':
-                        break
-                    if msg_type == 'execute_input':
-                        execution_count = dsafe(content, 'execution_count')
-                    # message is one of the output types for the Notebook
-                    if msg_type in self._output_msg_types and type(content) == dict:
-                        messages.append({ 'output_type': msg_type, **content })
-            except KeyboardInterrupt:
-                break
-        return execution_count, messages
+            msg = await kc.get_iopub_msg()
+
+            kernel_id = str(dsafe(msg, 'parent_header', 'msg_id'))
+            jupyviv_id = id_kernel2jupyviv.get(kernel_id)
+            if jupyviv_id is None:
+                continue
+
+            msg_type = dsafe(msg, 'msg_type')
+            content = dsafe(msg, 'content')
+            if msg_type == 'status':
+                state = str(dsafe(content, 'execution_state'))
+                await send_queue.put(Message(jupyviv_id, 'status', state))
+                continue
+
+            # TODO: figure out & react to other messages
+            _logger.info(f'Received message: {msg_type} with content: {content}')
+
+    async def _execute(message: Message):
+        kernel_id = kc.execute(message.args)
+        id_kernel2jupyviv[kernel_id] = message.id
+
+    handler = AsyncMessageHandler({
+        'execute': _execute
+    })
+
+    return (handler, _handle_kernel_messages)
