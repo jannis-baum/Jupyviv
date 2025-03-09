5e3ab5965787de2f3f1c43a4bb505d8f3e413436
Jannis Baum <jannis.baum@gmail.com>
2025-03-09 12:12:05 +0100

    feat(#1): websocket transport client

--- jupyviv/shared/transport/websocket.py
+++ jupyviv/shared/transport/websocket.py
@@ -1,6 +1,7 @@
 import asyncio
 from typing import Awaitable, Callable
 
+from websockets.asyncio.client import ClientConnection, connect
 from websockets.asyncio.server import ServerConnection, serve
 
 from jupyviv.shared.messages import AsyncMessageHandler, Message
@@ -19,6 +20,27 @@ def setup_server(port: int, handler: AsyncMessageHandler) -> Callable[[Message],
             await server.serve_forever()
 
     asyncio.run(run_server())
+
+    async def send_message(message: Message):
+        if websocket_ref is not None:
+            await websocket_ref.send(message.to_str())
+
+    return send_message
+
+def setup_client(host: str, port: int, handler: AsyncMessageHandler) -> Callable[[Message], Awaitable[None]]:
+    websocket_ref: ClientConnection | None = None
+
+    async def consumer(websocket: ClientConnection):
+        nonlocal websocket_ref
+        websocket_ref = websocket
+        async for message in websocket:
+            await handler.handle(str(message))
+
+    async def run_client():
+        async with connect(f'ws://{host}:{port}') as websocket:
+            await consumer(websocket)
+
+    asyncio.run(run_client())
 
     async def send_message(message: Message):
         if websocket_ref is not None:
