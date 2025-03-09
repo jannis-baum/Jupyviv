5aa8194f208b32d4f02879e2839ded36e6b7f1ed
Jannis Baum <jannis.baum@gmail.com>
2025-03-09 17:51:12 +0100

    refactor(#1): messages without arg list

--- src/jupyviv/shared/messages.py
+++ src/jupyviv/shared/messages.py
@@ -15,10 +15,10 @@ class MessageUnknownError(JupyVivError):
 T = TypeVar("T")
 
 class Message:
-    def __init__(self, id: int, command: str, args: list[str]):
+    def __init__(self, id: int, command: str, args: str):
         self.id = id
         self.command = command
-        self._args = args
+        self.args = args
 
     @staticmethod
     def from_str(message_str: str):
@@ -29,16 +29,10 @@ class Message:
             id = int(parts[0])
         except ValueError:
             raise MessageFormatError(message_str)
-        return Message(id, parts[1], parts[2:])
-
-    def arg(self, index: int, constructor: Callable[[str], T]) -> T:
-        try:
-            return constructor(self._args[index])
-        except (ValueError, IndexError):
-            raise MessageFormatError(self.to_str())
+        return Message(id, parts[1], ' '.join(parts[2:]))
 
     def to_str(self):
-        return ' '.join([str(self.id), self.command, *self._args])
+        return ' '.join([str(self.id), self.command, self.args])
 
 class MessageHandler:
     def __init__(self, handlers: dict[str, Callable[[Message], None]]):
