74e60b05c6aede0d0852b38c9e732b2a7c88bbc7
Jannis Baum <jannis.baum@gmail.com>
2025-03-09 20:49:46 +0100

    feat(#1): send outputs

--- src/jupyviv/agent/kernel.py
+++ src/jupyviv/agent/kernel.py
@@ -1,4 +1,6 @@
+import json
 from typing import Awaitable, Callable
+
 from jupyter_client.manager import start_new_async_kernel
 
 from jupyviv.shared.logs import get_logger
@@ -6,6 +8,7 @@ from jupyviv.shared.messages import AsyncMessageHandler, AsyncMessageQueue, Mess
 from jupyviv.shared.utils import dsafe
 
 _logger = get_logger(__name__)
+_output_msg_types = ['execute_result', 'display_data', 'stream', 'error']
 
 # returns message handler & runner for kernel
 async def setup_kernel(name: str, send_queue: AsyncMessageQueue) -> tuple[AsyncMessageHandler, Callable[[], Awaitable[None]]]:
@@ -26,13 +29,23 @@ async def setup_kernel(name: str, send_queue: AsyncMessageQueue) -> tuple[AsyncM
 
             msg_type = dsafe(msg, 'msg_type')
             content = dsafe(msg, 'content')
+
             if msg_type == 'status':
                 state = str(dsafe(content, 'execution_state'))
                 await send_queue.put(Message(jupyviv_id, 'status', state))
                 continue
 
-            # TODO: figure out & react to other messages
-            _logger.info(f'Received message: {msg_type} with content: {content}')
+            if msg_type == 'execute_input':
+                execution_count = str(dsafe(content, 'execution_count'))
+                await send_queue.put(Message(jupyviv_id, 'execute_input', execution_count))
+                continue
+
+            if msg_type in _output_msg_types and type(content) == dict:
+                data = json.dumps({ 'output_type': msg_type, **content })
+                await send_queue.put(Message(jupyviv_id, 'output', data))
+                continue
+
+            _logger.info(f'Received unknown message: {msg_type} with content: {content}')
 
     async def _execute(message: Message):
         kernel_id = kc.execute(message.args)
