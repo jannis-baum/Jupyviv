89b54189c34baaa1ad14e1b39eb375cbfdfebf03
Jannis Baum <jannis.baum@gmail.com>
2025-03-13 12:11:35 +0100

    feat(#1): handler main run loop

--- src/jupyviv/handler/__init__.py
+++ src/jupyviv/handler/__init__.py
@@ -1,10 +1,26 @@
+import asyncio
+import sys
+
+from jupyviv.handler.sync import JupySync
+from jupyviv.handler.vivify import viv_open
+from jupyviv.shared.messages import Message, MessageHandler, new_queue
+from jupyviv.shared.transport.iostream import run
 from jupyviv.shared.utils import Subparsers
 
 async def _main(notebook: str, agent_address: str | None):
-    pass
+    with JupySync(notebook) as jupy_sync:
+        viv_open(notebook)
+
+        send_queue = new_queue()
+        recv_handler = MessageHandler({})
+
+        try:
+            await run(recv_handler, send_queue, sys.stdin, sys.stdout)
+        except asyncio.CancelledError: # keyboard interrupt
+            pass
 
 def _cli(args):
-    pass
+    asyncio.run(_main(args.notebook, args.agent))
 
 def setup_handler_args(subparsers: Subparsers):
     parser = subparsers.add_parser('handler', help='Run the handler')
