f51a1c83a6419ac1cd64aac0c805d900d297eca4
Jannis Baum <jannis.baum@gmail.com>
2025-04-13 17:13:51 +0200

    feat: ensure shut down

--- src/jupyviv/shared/lifetime.py
+++ src/jupyviv/shared/lifetime.py
@@ -1,14 +1,19 @@
 import asyncio
 import os
 import signal
+import sys
 
 import psutil
+
+from jupyviv.shared.logs import get_logger
+
+_logger = get_logger(__file__)
 
 def _graceful_shutdown():
     os.kill(os.getpid(), signal.SIGINT)
 
 # monitor if parent process is still alive; if not, gracefully shut down
-async def shutdown_with_parent(interval=1):
+async def shutdown_with_parent(interval=1, shutdown_timeout=10):
     ppid = os.getppid()
     try:
         parent = psutil.Process(ppid)
@@ -18,5 +23,9 @@ async def shutdown_with_parent(interval=1):
 
     while True:
         if not parent.is_running() or parent.status() == psutil.STATUS_ZOMBIE:
+            _logger.info('Parent process died, shutting down')
             os.kill(os.getpid(), signal.SIGINT)
+            await asyncio.sleep(shutdown_timeout)
+            _logger.warning('Failed to gracefully shut down without deadline, exiting')
+            sys.exit(1)
         await asyncio.sleep(interval)
