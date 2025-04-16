d3494ab43eb6d2ede00a111d7b4e0586ddb3507b
Jannis Baum <jannis.baum@gmail.com>
2025-04-16 19:21:39 +0200

    chore(#3): organize metadata retrieval

--- src/jupyviv/handler/new.py
+++ src/jupyviv/handler/new.py
@@ -9,21 +9,21 @@ async def create_notebook(path: str, agent_addr: str):
     if not path.endswith('.ipynb'):
         raise JupyVivError('New Notebook needs to end in ".ipynb"')
 
+    # metadata is retrieved into queue so we can await it
     metadata_queue = asyncio.Queue()
     async def recv_metadata(message: Message):
         await metadata_queue.put(json.loads(message.args))
 
+    # agent communication setup
     send_queue = new_queue()
-    await send_queue.put(Message('new', 'get_metadata'))
     handler = MessageHandler({ 'metadata': recv_metadata })
-
     async def run_communication():
-        try:
-            await run_client(agent_addr, handler, send_queue)
-        except:
-            pass
+        try: await run_client(agent_addr, handler, send_queue)
+        except: pass
     socket_task = asyncio.create_task(run_communication())
 
+    # get metadata
+    await send_queue.put(Message('new', 'get_metadata'))
     try:
         metadata = await asyncio.wait_for(metadata_queue.get(), timeout=10)
     except:
