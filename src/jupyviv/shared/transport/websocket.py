7c08c10186077ef06e49dba39a3b332225bac04c
Jannis Baum <jannis.baum@gmail.com>
2025-04-16 19:57:09 +0200

    fix(#3): agent reconnection & termination

--- src/jupyviv/shared/transport/websocket.py
+++ src/jupyviv/shared/transport/websocket.py
@@ -34,23 +34,28 @@ async def _connection_handler(
                     message = await send_queue.get()
                 _logger.debug(f'Websocket sending message: {message}')
                 await websocket.send(message.to_str())
-            except ConnectionClosed:
+            except (ConnectionClosed, asyncio.CancelledError):
                 dropped_message.message = message
                 break
             except Exception as e:
                 _logger.error(f'Send error {type(e)}: {e}')
 
     async def _receiver():
-        async for message in websocket:
-            try:
-                _logger.debug(f'IO received message string: {str(message)}')
-                await recv_handler.handle(str(message))
-            except ConnectionClosed:
-                break
-            except Exception as e:
-                _logger.error(f'Receive error {type(e)}: {e}')
+        try:
+            async for message in websocket:
+                try:
+                    _logger.debug(f'IO received message string: {str(message)}')
+                    await recv_handler.handle(str(message))
+                except Exception as e:
+                    _logger.error(f'Receive error {type(e)}: {e}')
+        except (ConnectionClosed, asyncio.CancelledError):
+            pass
 
-    await asyncio.gather(_sender(), _receiver())
+    sender_task = asyncio.create_task(_sender())
+    # receiver exits on ConnectionClose, we use that to cancel sender as well
+    await _receiver()
+    sender_task.cancel()
+    await sender_task
 
 default_port = 8000
 
