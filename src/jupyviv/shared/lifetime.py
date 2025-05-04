e68d9a62d93c59d2d9bbd3da0fb75f03fc369c84
Jannis Baum <jannis.baum@gmail.com>
2025-05-04 11:28:52 +0200

    feat(#3): reusable shutdown helper

--- src/jupyviv/shared/lifetime.py
+++ src/jupyviv/shared/lifetime.py
@@ -7,15 +7,18 @@ from jupyviv.shared.logs import get_logger
 
 _logger = get_logger(__file__)
 
+async def shutdown(timeout=1):
+    os.kill(os.getpid(), signal.SIGINT)
+    await asyncio.sleep(timeout)
+    _logger.warning('Failed to gracefully shut down without deadline, exiting')
+    sys.exit(1)
+
 # monitor if parent process is still alive; if not, gracefully shut down
-async def shutdown_with_parent(interval=1, shutdown_timeout=10):
+async def shutdown_with_parent(interval=1, shutdown_timeout=1):
     ppid = os.getppid()
 
     while True:
         if os.getppid() != ppid or ppid == 1:
             _logger.info('Parent process died, shutting down')
-            os.kill(os.getpid(), signal.SIGINT)
-            await asyncio.sleep(shutdown_timeout)
-            _logger.warning('Failed to gracefully shut down without deadline, exiting')
-            sys.exit(1)
+            await shutdown(shutdown_timeout)
         await asyncio.sleep(interval)
