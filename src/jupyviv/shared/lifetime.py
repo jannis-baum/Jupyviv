79d80bdfda20965ee3661808d82ccc9c32ce68f1
Jannis Baum <jannis.baum@gmail.com>
2025-05-04 10:19:39 +0200

    refactor(#3): simplify parent died shutdown

--- src/jupyviv/shared/lifetime.py
+++ src/jupyviv/shared/lifetime.py
@@ -3,26 +3,16 @@ import os
 import signal
 import sys
 
-import psutil
-
 from jupyviv.shared.logs import get_logger
 
 _logger = get_logger(__file__)
 
-def _graceful_shutdown():
-    os.kill(os.getpid(), signal.SIGINT)
-
 # monitor if parent process is still alive; if not, gracefully shut down
 async def shutdown_with_parent(interval=1, shutdown_timeout=10):
     ppid = os.getppid()
-    try:
-        parent = psutil.Process(ppid)
-    except psutil.NoSuchProcess:
-        _graceful_shutdown()
-        return
 
     while True:
-        if not parent.is_running() or parent.status() == psutil.STATUS_ZOMBIE:
+        if os.getppid() != ppid or ppid == 1:
             _logger.info('Parent process died, shutting down')
             os.kill(os.getpid(), signal.SIGINT)
             await asyncio.sleep(shutdown_timeout)
