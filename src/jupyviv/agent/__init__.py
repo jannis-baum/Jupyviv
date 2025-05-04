c9bcb13a4711f03136aea04ca68b7951d7a3eeba
Jannis Baum <jannis.baum@gmail.com>
2025-05-04 11:10:40 +0200

    fix: handle critical agent errors

--- src/jupyviv/agent/__init__.py
+++ src/jupyviv/agent/__init__.py
@@ -3,22 +3,29 @@ import subprocess
 import sys
 
 from jupyviv.agent.kernel import setup_kernel
+from jupyviv.shared.logs import get_logger
 from jupyviv.shared.messages import MessageHandler, new_queue
 from jupyviv.shared.transport.websocket import default_port, run_server
 from jupyviv.shared.utils import Subparsers
 
+_logger = get_logger(__name__)
+
 async def main(args):
-    send_queue = new_queue()
-    handlers, run_kernel = await setup_kernel(args.kernel_name, send_queue)
-
-    recv_handler = MessageHandler(handlers)
-    server_task = asyncio.create_task(run_server(args.port, recv_handler, send_queue))
-
     try:
-        await run_kernel()
-    except asyncio.CancelledError: # keyboard interrupt
-        server_task.cancel()
-        await server_task
+        send_queue = new_queue()
+        handlers, run_kernel = await setup_kernel(args.kernel_name, send_queue)
+
+        recv_handler = MessageHandler(handlers)
+        server_task = asyncio.create_task(run_server(args.port, recv_handler, send_queue))
+
+        try:
+            await run_kernel()
+        except asyncio.CancelledError: # keyboard interrupt
+            server_task.cancel()
+            await server_task
+    except Exception as e:
+        _logger.critical(e)
+        return 1
 
 def launch_as_subprocess(kernel_name: str, log_level: str, outlive_parent: bool) -> subprocess.Popen:
     return subprocess.Popen([
